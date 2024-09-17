fe9c4389030888d3efddbd7bda2527309823129b
bradleymarch <bradleymarch@gmail.com>
2024-09-17 11:08:50 -0400

    sync indentation

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -8,50 +8,50 @@ import 'react-tabs/style/react-tabs.css'
 function Landing() {
    const [superheroes, setSuperheroes] = useState([])
 
-  useEffect(() => {
-    const url = 'https://tppublic.blob.core.windows.net/test-data/super-heroes.json'
-    axios.get(url).then((response) => {
-      setSuperheroes(response.data)
-    })
-    .catch((error) => {
-      return error
-    })
-  }, [])
+   useEffect(() => {
+      const url = 'https://tppublic.blob.core.windows.net/test-data/super-heroes.json'
+         axios.get(url).then((response) => {
+         setSuperheroes(response.data)
+      })
+      .catch((error) => {
+         return error
+      })
+   }, [])
 
-  return (
-   <Tabs>        
-      <TabList>
-         <Tab>Superheroes List</Tab>
-         <Tab>Superheroes Grid</Tab>
-      </TabList>
-      <TabPanel>
-         <ul className="superhero-list">
+   return (
+      <Tabs>        
+         <TabList>
+            <Tab>Superheroes List</Tab>
+            <Tab>Superheroes Grid</Tab>
+         </TabList>
+         <TabPanel>
+            <ul className="superhero-list">
+               {
+                  superheroes.map((hero, index) => (
+                     <ListItem
+                        key={index}
+                        id={hero.id}
+                        name={hero.name}
+                        realName={hero.biography['full-name']}
+                        columnAlign={['left', 'left']}
+                     />
+                  ))
+               }
+            </ul>
+         </TabPanel>
+         <TabPanel>
             {
                superheroes.map((hero, index) => (
-                  <ListItem
+                  <GridItem
                      key={index}
-                     id={hero.id}
                      name={hero.name}
-                     realName={hero.biography['full-name']}
-                     columnAlign={['left', 'left']}
+                     imageSrc={hero.image.url}
                   />
                ))
             }
-         </ul>
-      </TabPanel>
-      <TabPanel>
-         {
-            superheroes.map((hero, index) => (
-               <GridItem
-                  key={index}
-                  name={hero.name}
-                  imageSrc={hero.image.url}
-               />
-            ))
-         }
-      </TabPanel>
-   </Tabs>
-  )
+         </TabPanel>
+      </Tabs>
+   )
 }
 
 export default Landing
--- src/components/shared/ListItem.jsx
+++ src/components/shared/ListItem.jsx
@@ -12,20 +12,20 @@ const ListItem = (props) => {
       <>
          <li className="superhero-list-item">
             <table>
-            <tbody>
-               <tr>
-                  <td className="superhero-data-cell" align={columnAlign[0]}>ID:</td>
-                  <td className="superhero-data-cell" align={columnAlign[1]}>{id}</td>
-               </tr>
-               <tr>
-                  <td className="superhero-data-cell" align={columnAlign[0]}>Name:</td>
-                  <td className="superhero-data-cell" align={columnAlign[1]}>{name}</td>
-               </tr>
-               <tr>
-                  <td className="superhero-data-cell" align={columnAlign[0]}>Real Name:</td>
-                  <td className="superhero-data-cell" align={columnAlign[1]}>{realName}</td>
-               </tr>
-            </tbody>
+               <tbody>
+                  <tr>
+                     <td className="superhero-data-cell" align={columnAlign[0]}>ID:</td>
+                     <td className="superhero-data-cell" align={columnAlign[1]}>{id}</td>
+                  </tr>
+                  <tr>
+                     <td className="superhero-data-cell" align={columnAlign[0]}>Name:</td>
+                     <td className="superhero-data-cell" align={columnAlign[1]}>{name}</td>
+                  </tr>
+                  <tr>
+                     <td className="superhero-data-cell" align={columnAlign[0]}>Real Name:</td>
+                     <td className="superhero-data-cell" align={columnAlign[1]}>{realName}</td>
+                  </tr>
+               </tbody>
             </table>
          </li>
          <hr />
